2e0419bc33e261ba643fb5eb84edeb2fe1269ba6
Bambooomatic <bambynek.pawel@gmail.com>
2019-11-14 17:01:56 +0100

    wracam po przerwie! poprawiona galeria, dodane zjdęcia

---
+++ myProtfolio/src/js/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import './../sass/style.scss'; // adres do głównego pliku SASS
+
+// import img from '../images/';
+
+const App = () => {
+    return(
+        <>
+        hello
+        </>
+    )
+};
+
+ReactDOM.render(<App/>, document.getElementById("app"));
